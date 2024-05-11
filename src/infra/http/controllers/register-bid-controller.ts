363d6505b782fcdbc4f3870546f29f17213150a4
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-11 15:50:09 -0300

    feat: add register bid controller

---
+++ src/infra/http/controllers/register-bid-controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express'
+import { registerBidBodySchema } from '../schemas/register-bid-body-schema'
+import { makeRegisterBidUseCase } from '../factories/make-register-bid-use-case'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+
+export async function registerBidController(
+  request: Request,
+  response: Response,
+) {
+  const { licensePlate } = request.params
+  const bidObject = registerBidBodySchema.parse(request.body)
+
+  const registerUseCase = makeRegisterBidUseCase()
+
+  const result = await registerUseCase.execute({
+    licensePlate,
+    bid: bidObject,
+  })
+
+  if (result.isLeft()) {
+    const error = result.value
+
+    switch (error.constructor) {
+      case ResourceNotFoundError:
+        return response.status(404).send({
+          message: error.message,
+        })
+      default:
+        return response.status(400).send({
+          message: error.message,
+        })
+    }
+  }
+
+  return response.status(201).send()
+}
